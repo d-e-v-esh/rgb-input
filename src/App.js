7dd10aa4f9175e4f2f009dd2d52840d21b0d70d8
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2020-12-15 23:21:16 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.scss";
+import { connect } from "react-redux";
+import { controlRed, controlGreen, controlBlue } from "./store/color";
 
-function App() {
+const mapDispatch = { controlRed, controlGreen, controlBlue };
+
+const App = () => {
+  const redDragHandler = (e) => {
+    const redValue = e.target.value;
+    console.log(redValue);
+    controlRed(redValue);
+  };
+
+  const greenDragHandler = (e) => {
+    //e.target.value
+  };
+
+  const blueDragHandler = (e) => {
+    //e.target.value
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="colouredSquare" />
+      </div>
+
+      <div className="sliderInputs">
+        <div className="containerRed">
+          <label htmlFor="">
+            Red
+            <input
+              type="range"
+              min={0}
+              max={255}
+              id="redInput"
+              onChange={redDragHandler}
+            />
+          </label>
+        </div>
+
+        <div className="containerGreen">
+          <label htmlFor="">
+            Green
+            <input
+              type="range"
+              min={0}
+              max={255}
+              id="greenInput"
+              onChange={greenDragHandler}
+            />
+          </label>
+        </div>
+
+        <div className="containerBlue">
+          <label htmlFor="">
+            Blue
+            <input
+              type="range"
+              min={0}
+              max={255}
+              id="blueInput"
+              onChange={blueDragHandler}
+            />
+          </label>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
